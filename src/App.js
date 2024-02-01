f587198afd54ed38b45916243339ce19bc0d59f6
movarnell <movarnell@gmail.com>
2024-02-01 17:16:20 -0600

    Refactor App component to fetch and display seats

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import './App.css';
-import Header from './Components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './Components/Navigation';
+import "./App.css";
+import Header from "./Components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./Components/Navigation";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [seats, setSeats] = useState([]);
+
+  useEffect(() => {
+    getSeats();
+  }, []);
+
+  const getSeats = async () => {
+    const response = await fetch(
+      "https://65bc1cf852189914b5bd9bf1.mockapi.io/seats"
+    );
+    const data = await response.json();
+    setSeats(data);
+  };
+
   return (
-    <div className="container-fluid bg-dark">
+    <div className='container-fluid bg-dark'>
       <Navigation />
-      <Header/>
-      <p>This is new to demo, DELETE ME</p>
+      <Header />
+      <div className='container'>
+        <div className='row'>
+          {seats.map((seat) => (
+            <div className='col-4 border border-1 m-1 text-white' key={seat.id}>
+              <p>Seat Number:{seat.seatDescription}</p>
+              <p>Available: {seat.seatAvailable ? "Yes": "Sold"}</p>
+              <p>Price: ${seat.seatPrice}</p>
+              <p>{seat.disabled ? "Disabled": ""}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
