1f28d3cc8ce828dfac2a8e636c11cc5c9e1f5602
movarnell <movarnell@gmail.com>
2024-02-01 16:26:02 -0600

    added the navbar and it works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <div className="container-fluid bg-dark">
+      <Navigation />
       <Header/>
     </div>
   );
---
+++ src/Components/Navigation.js
@@ -0,0 +1,35 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Logo from "../Assets/logo.png";
+
+function Navigation() {
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Brand href='#home'><img src={Logo} alt="Fan-Prom-Ango logo" width="40px" height="auto"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.2'>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='#action/3.4'>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
