a39b5a9f467faa558971c3a77e64348eda4cb387
movarnell <movarnell@gmail.com>
2024-02-01 14:53:40 -0600

    Add Bootstrap and React Bootstrap libraries

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import TitleImg from '../Assets/Fan-Prom-Ango.png';
+
+function Header(){
+    return (
+        <div>
+            <img src={TitleImg} alt="Fan-Prom-Ango Logo" width="100%" height= "auto" />
+        </div>
+    )
+}
+export default Header;
