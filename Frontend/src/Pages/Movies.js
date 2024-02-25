c5f8c90c8b30d9637a89ac3a95594c1457477e9b
Bob <151000427+WilkBob@users.noreply.github.com>
2024-02-25 00:46:00 -0800

    Update movie to carousel

--- Frontend/src/Pages/Movies.js
+++ Frontend/src/Pages/Movies.js
@@ -1,37 +1,54 @@
 import React from 'react'
-import { Card, Button, Row, Col } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom';
+import { Carousel, Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
 
-const Movies = ({theaterID, setMovieID, movies, theaters}) => {
-    
-const history = useHistory();
-    const redirect = (path) =>{ 
-        history.push(path);
-      }
+const Movies = ({ theaterID, setMovieID, movies, theaters }) => {
+    const history = useHistory()
+    const redirect = (path) => {
+        history.push(path)
+    }
 
     return (
         <>
-        <Row className='fade-in text-light'>
-            <h2>Now Showing {theaterID > 0 && ['At ', theaters[theaterID-1].name]}</h2>
-        </Row>
-        <Row xs={1} md={2} xl={3} className="fade-in g-4">
-            {movies.map((movie, index) => (
-                <Col  key={index}>
-                    <Card>
-                        
-                        
-                        <Card.Body>
-                            <Card.Title>{movie.title}</Card.Title>
-                            <Card.Text>{movie.description}</Card.Text>
-                            {theaterID>0 ? 
-                            <Button variant='success' onClick={() => {setMovieID(movie.id); redirect('/seat')} }>Select Seats</Button>
-                                :<Button variant="primary" onClick={() => {setMovieID(movie.id); redirect('/theaters')}} >Select a Theater</Button>
-                            }
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+            <h2 className='fade-in text-light'>
+                Now Showing {theaterID > 0 && ['At ', theaters[theaterID - 1].name]}
+            </h2>
+            <Carousel className="fade-in">
+                {movies.map((movie, index) => (
+                    <Carousel.Item key={index}>
+                        <img
+                            className="d-block w-100"
+                            src={`https:picsum.photos/800/400?random=${index}`}
+                            alt={movie.title}
+                        />
+                        <Carousel.Caption style={{backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'white', borderRadius: '5px'}}>
+                            <h3>{movie.title}</h3>
+                            <p style={{textOverflow:'ellipsis'}}>{movie.description}</p>
+                            {theaterID > 0 ? (
+                                <Button
+                                    variant='light'
+                                    onClick={() => {
+                                        setMovieID(movie.id)
+                                        redirect('/seat')
+                                    }}
+                                >
+                                    Select Seats
+                                </Button>
+                            ) : (
+                                <Button
+                                    variant="light"
+                                    onClick={() => {
+                                        setMovieID(movie.id)
+                                        redirect('/theaters')
+                                    }}
+                                >
+                                    Select a Theater
+                                </Button>
+                            )}
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
         </>
     )
 }
